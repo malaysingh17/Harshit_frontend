c5039028f65a0d4716a70cd406a8743b86a33172
Harshit singh <107110456+malaysingh17@users.noreply.github.com>
2023-04-23 23:42:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import List from "./list";
+
+function App() {
+  return (
+    <>
+      <List
+        items={[
+          { text: "Hi" },
+          { text: "steeleye assignment done" },
+          { text: "This side harshit singh" },
+          { text: "i am very honest person" }
+        ]}
+      />
+    </>
+  );
+}
+
+export default App;
